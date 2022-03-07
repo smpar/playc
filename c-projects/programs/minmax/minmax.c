a28f590316b8c071ee6b934f107f4e70b36392e0
smpar <smpar@users.noreply.github.com>
2022-03-07 20:54:18 +0200

    Add argument handling on minmax

--- c-projects/programs/minmax/minmax.c
+++ c-projects/programs/minmax/minmax.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 #include "stats.h"
 
 int main(int argc, char* argv[]) {
-    int array[] = {9, 17, 3, 4, 9};
-    int array1[] = {};
+    //int array[] = {9, 17, 3, 4, 9};
+    //size_t size = sizeof(array)/sizeof(array[0]);
+    //int array1[] = {};
 
-    size_t size = sizeof(array)/sizeof(array[0]);
+    printf("argc: %d\n", argc);
+    for (int i = 0; i < argc; i++){
+        printf("argv[%d]: %s\n", i, argv[i]);
+    }
+
+    int size = argc - 1;
+    int array[size];
+
+    for(int i = 0; i < size; i++)
+        array[i] = atoi(argv[i+1]);
+
     int max = find_max(array, size);
     printf("Max number: %d\n", max);
 }
