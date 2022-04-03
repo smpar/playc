81efa1b5fd057e205ac795a4627a4757f66a0813
smpar <smpar@users.noreply.github.com>
2022-04-03 10:39:51 +0300

    Initialize the project
    Generated by start.spring.io with dependencies:
    - Spring Web
    - Thymeleaf

---
+++ java-projects/spring-secure-web-app/src/main/java/com/example/springsecurewebapp/SpringSecureWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecureWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecureWebAppApplication.class, args);
+	}
+
+}
---
+++ java-projects/spring-secure-web-app/src/test/java/com/example/springsecurewebapp/SpringSecureWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecureWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
